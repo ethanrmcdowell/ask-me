478fe0134c50168f8dcb8dd2811a5441ac36b9f9
Ethan McDowell <ethan.r.mcdowell@gmail.com>
2023-10-03 16:26:48 -0400

    more firebase, styling, form fields

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Firestore, collection, addDoc, collectionData, query, getDocs, }  from '@angular/fire/firestore';
+import { FormControl } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -7,15 +9,26 @@ import { Firestore, collection, addDoc, collectionData, query, getDocs, }  from 
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ask-me';
-
   constructor(private firestore: Firestore) {
     this.getQuestions();
   }
 
-  submitQuestion(f: any) {
+  title = 'ask-me';
+  userEmail: any;
+  userName: any;
+  userQuestion: any;
+  questionArray: any = [];
+
+  submitQuestion() {
+    let formData = {
+      email: this.userEmail,
+      name: this.userName,
+      question: this.userQuestion,
+      timestamp: this.getDate(),
+    }
+
     const collectionInstance = collection(this.firestore, 'questions');
-    addDoc(collectionInstance, f.value).then(() => {
+    addDoc(collectionInstance, formData).then(() => {
       console.log('Data successfully saved!');
     })
     .catch((error) => {
@@ -23,13 +36,26 @@ export class AppComponent {
     })
   }
 
+  scrollDown() {
+    const element = document.querySelector("#qContainer");
+    element?.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  }
+
   async getQuestions() {
     const q = query(collection(this.firestore, 'questions'));
-
     const  querySnapshot = await getDocs(q);
 
     querySnapshot.forEach((doc) => {
-      console.log("DATA ->", doc.data());
-    })
+      this.questionArray.push(doc.data());
+    });
+    console.log("QUESTIONS ->", this.questionArray);
+  }
+
+  getDate() {
+    const date = new Date();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    const year = date.getFullYear();
+    return `${month}/${day}/${year}`;
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,11 @@ import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button'
+
 
 @NgModule({
   declarations: [
@@ -14,8 +19,12 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     FormsModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideFirestore(() => getFirestore())
+    provideFirestore(() => getFirestore()),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
