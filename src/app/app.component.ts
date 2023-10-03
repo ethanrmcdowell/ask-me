4fa8ea8b8faaa59e8b63461d0db32f7c58d0814f
Ethan McDowell <ethan.r.mcdowell@gmail.com>
2023-10-03 14:55:28 -0400

    setup firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Firestore, collection, addDoc, collectionData, query, getDocs, }  from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ask-me';
+
+  constructor(private firestore: Firestore) {
+    this.getQuestions();
+  }
+
+  submitQuestion(f: any) {
+    const collectionInstance = collection(this.firestore, 'questions');
+    addDoc(collectionInstance, f.value).then(() => {
+      console.log('Data successfully saved!');
+    })
+    .catch((error) => {
+      console.error(error);
+    })
+  }
+
+  async getQuestions() {
+    const q = query(collection(this.firestore, 'questions'));
+
+    const  querySnapshot = await getDocs(q);
+
+    querySnapshot.forEach((doc) => {
+      console.log("DATA ->", doc.data());
+    })
+  }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'ask-me-1b00c',
+    appId: '1:740402941189:web:cc5f63b96645d697f1bb6c',
+    storageBucket: 'ask-me-1b00c.appspot.com',
+    apiKey: 'AIzaSyBiDZmjOjmkL_l-WbJSd_UtPO8sjt7HQ4k',
+    authDomain: 'ask-me-1b00c.firebaseapp.com',
+    messagingSenderId: '740402941189',
+  },
+};
